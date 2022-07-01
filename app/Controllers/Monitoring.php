24ea8991e976761af20e78a031817916a1ba0970
fithoriqil azziz <fithoriqil.azziz1033@gmail.com>
2022-07-01 15:09:14 +0700

    bug fix

--- app/Controllers/Monitoring.php
+++ app/Controllers/Monitoring.php
@@ -599,7 +599,7 @@ class Monitoring extends BaseController
 
 			$pdfFile = $dirPdf . $fileName . '.pdf';
 
-			// unlink(WRITEPATH . 'report/monev/' . $fileName . '.docx');
+			unlink(WRITEPATH . 'report/monev/' . $fileName . '.docx');
 		}
 		catch (\BadMethodCallException $e)
 		{
@@ -620,7 +620,7 @@ class Monitoring extends BaseController
 	{
 		if (! empty($_POST))
 		{
-			if (unlink('/var/www/cadas.comcd /public/' . $_POST['fileName']))
+			if (unlink('/var/www/cadas.com/public/assets/report/monev' . $_POST['fileName']))
 			{
 				echo json_encode('file berhasil dihapus');
 			}
@@ -629,42 +629,5 @@ class Monitoring extends BaseController
 				echo json_encode('file gagal dihapus');
 			}
 		}
-	}
-
-	public function test_phpword(){
-		$data     = $this->model->getMonumById(60);
-		$template = WRITEPATH . 'report/monev/template/template_monev_umum_moncer.docx';
-		$dirDocx  = $dirPdf = 'assets/report/monev/';
-		$thick    = mb_convert_encoding('&#x2714;', 'UTF-8', 'HTML-ENTITIES');
-		// $fileDir = 'C:\xampp\htdocs\DashboardTPB\assets\upload\monev\report_docx\\';
-
-		$headerLaporan = $data['laporan'];
-		$isiLaporan    = $data['isi'];
-
-		// \PhpOffice\PhpWord\Settings::setOutputEscapingEnabled(true);
-		$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor($template);
-		$templateProcessor->setValue('nama_perusahaan', $headerLaporan['nama_perusahaan']);
-		$templateProcessor->setValue('alamat', $headerLaporan['alamat']);
-		$templateProcessor->setValue('tanggal', date('d-m-Y', strtotime($headerLaporan['tanggalLaporan'])));
-		$templateProcessor->setValue('kesimpulan', $headerLaporan['kesimpulan']);
-		$templateProcessor->setValue('nama', $headerLaporan['NamaPegawai']);
-
-		for ($i = 0; $i < count($isiLaporan); $i++)
-		{
-			$templateProcessor->setValue('ket' . $isiLaporan[$i]['item'], $isiLaporan[$i]['keterangan']);
-		}
-
-		$fileName = 'Laporan_Moncer_' . $headerLaporan['idPerusahaan'] . '_' . date('d-m-Y', strtotime($headerLaporan['tanggalLaporan']));
-
-		$report = WRITEPATH . 'report/monev/' . $fileName . '.docx';
-
-		$templateProcessor->saveAs($report);
-
-		shell_exec('export HOME=/tmp && libreoffice --headless --convert-to pdf:writer_pdf_Export --outdir /var/www/cadas.com/public/assets/report/monev /var/www/cadas.com/writable/report/monev/' . $fileName . '.docx');
-
-		$pdfFile = $dirPdf . $fileName . '.pdf';
-
-		// unlink(WRITEPATH . 'report/monev/' . $fileName . '.docx');
-
 	}
 }
